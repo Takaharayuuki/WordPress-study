d6b34a3b47244e9a6aba270a33784984454798c0
Takaharayuuki <yuuki.barber@gmail.com>
2021-03-31 17:31:11 +0900

    footerのナビ調整

--- wp-content/themes/tf-30/footer.php
+++ wp-content/themes/tf-30/footer.php
@@ -4,15 +4,17 @@
 			<div class="footer-logo"><a href="/">TF-30</a></div><!-- /footer-logo -->
 			<div class="footer-sub">サブタイトルが入りますサブタイトルが入ります</div><!-- /footer-sub -->
 
-			<nav class="footer-nav">
-				<ul class="footer-list">
-					<li class="menu-item"><a href="#">メニュー1</a></li>
-					<li class="menu-item"><a href="#">メニュー2</a></li>
-					<li class="menu-item"><a href="#">メニュー3</a></li>
-					<li class="menu-item"><a href="#">メニュー4</a></li>
-					<li class="menu-item"><a href="#">メニュー5</a></li>
-				</ul>
-			</nav>
+			<?php
+				wp_nav_menu(
+					array(
+						'depth' => 1,
+						'theme_location' => 'footer-menu',
+						'container' => 'nav',
+						'container_class' => 'footer-nav',
+						'menu_class' => 'footer-list',
+					)
+					);
+				?>
 
 		</div><!-- /inner -->
 	</div><!-- /footer-menu -->
